86448210edf6e72ab5404fc8fd5d7255ad5803c5
Alun Evans <alun@alunthomasevans.co.uk>
2017-10-13 11:59:49 +0200

    python script now evaluates correct node for test
    will_it_fall.py now correctly identifies the final output node of the graph. By calling eval on that node (line 119) we can test any axis to output a predicted angle. Next step is to save network and load/test in a separate script

--- tensor_flow/will_it_fall.py
+++ tensor_flow/will_it_fall.py
@@ -14,6 +14,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2' # stops tf warnings
 import tensorflow as tf
 import numpy as np
+from numpy import array
 from libs import utils
 
 print("Reading data...")
@@ -31,7 +32,9 @@ def readDataFile(input_file):
 	axes = np.asarray(axes_list)
 	angles = np.asarray(angles_list).reshape(len(angles_list),1)
 
-	axes = (axes - np.mean(axes)) / np.std(axes)
+	#here we DON'T normalize or subtract SD, as we would do with an image
+	#because we are going to be testing a single axis (as opposed to set of pixels)
+	#axes = (axes - np.mean(axes)) / np.std(axes)
 	return axes, angles
 
 axes, angles = readDataFile(TRAIN_FILE_NAME);
@@ -47,7 +50,7 @@ Y = tf.placeholder(tf.float32, [None, n_output], name='Y')
 
 n_neurons = [3, 64, 64, 64, 64, 64, 64, 1]
 
-current_input = X # start with just coords [4096, 2]
+current_input = X 
 for layer_i in range(1, len(n_neurons)): 
 	current_input, W = utils.linear(
 		x=current_input,
@@ -56,18 +59,7 @@ for layer_i in range(1, len(n_neurons)):
 		name='layer_' + str(layer_i))
 Y_pred = current_input
 
-
-
-#create linear layer
-#Y_pred, W = utils.linear(
-	#x=X,
-	#n_output=n_output,
-	#activation=tf.nn.relu, #softmax, tanh, sigmoid, relu
-	#name='layer1')
-
-#loss function
-#cost = tf.reduce_mean(distance(Y_pred, Y))
-#cost = tf.reduce_mean(tf.reduce_sum(tf.abs(Y - Y_pred), 1))
+#cost function simply takes mean of squared differences
 cost = tf.reduce_mean(tf.reduce_sum(tf.squared_difference(Y, Y_pred), 1))
 
 #optimizer
@@ -80,14 +72,12 @@ sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
 batch_size = 500
-n_epochs = 30
+n_epochs = 30 # after 26ish epochs the cost doesn't drop at all
 training_cost = 0
 for epochs_i in range(n_epochs):
-	#print("Epoch " + str(epochs_i))
 	idxs = np.random.permutation(range(len(axes))) #randomly shake up the array
 	n_batches = len(idxs) // batch_size
 	for batch_i in range(n_batches):
-		#print("Batch " + str(batch_i))
 		idxs_i = idxs[batch_i * batch_size : (batch_i + 1) * batch_size]
 		
 		sess.run(
@@ -100,24 +90,35 @@ for epochs_i in range(n_epochs):
 
 	#if epochs_i % 10 == 0:
 	print(epochs_i, training_cost)
+
 print("Done. Starting testing...")
-
 print( sess.run(cost, feed_dict={X: axes_t, Y: angles_t} ) )
+print("Done. Final test...")
 
-print("Done. Saving network...")
 
+#access the entire current graph
+g = tf.get_default_graph()
+names = [op.name for op in g.get_operations()]
+# for i in names:
+# 	print (i)
 
-#access graph
-# g = tf.get_default_graph()
-# names = [op.name for op in g.get_operations()]
-# print(names)
+#get the very first input node, should be of shape (?,3)
+input_name = names[0] + ':0'
+x = g.get_tensor_by_name(input_name)
 
-# input_name = names[0] + ':0'
-# x = g.get_tensor_by_name(input_name)
-# print(x)
-# output = g.get_tensor_by_name(names[-1] + ':0')
-# print(output)
+#layer_7/h is the very last node of graph, shape (?, 1)
+output = g.get_tensor_by_name('layer_7/h:0')
 
+print("Testing with [-0.7923625, 0.0, 0.6100528], result should be [26.60093]")
+
+# test axis
+an_input = array ( [-0.7923625, 0.0, 0.6100528] )
+a4d = an_input[np.newaxis] # convert to (1,3)
+
+#do the test by calling eval on output node
+print( output.eval(feed_dict={x: a4d}, session=sess) ) 
+
+#save the graph
 #saver = tf.train.Saver()
 #saver.save(sess, './wif')
 
